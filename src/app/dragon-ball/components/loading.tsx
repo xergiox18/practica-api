2e4b7832536d2346372fe3df9d1f7e00e57e53e7
Sergio Otalvaro <xergio1803@gmail.com>
2025-04-04 10:21:46 -0500

    boton

--- src/app/dragon-ball/components/loading.tsx
+++ src/app/dragon-ball/components/loading.tsx
@@ -2,169 +2,180 @@ import React from "react";
 
 const Loader: React.FC = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="space-y-6">
-        <div className="flex justify-center space-x-8">
-          <div className="loader">
-            <svg viewBox="0 0 150 150">
-              <rect x="15" y="15" width="120" height="120"></rect>
-              <text
-                x="50%"
-                y="60%"
-                textAnchor="middle"
-                fill="black"
-                fontSize="60"
-                fontWeight="bold"
-              >
-                D
-              </text>
-            </svg>
+    <div
+      className="flex items-center justify-center min-h-screen bg-orange-500"
+      style={{
+        backgroundImage:
+          "repeating-conic-gradient(from 0deg, orangered, transparent 10deg, orange 10deg, orangered 20deg)",
+        backgroundSize: "100% 100%",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: "orange",
+      }}
+    >
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="space-y-6">
+          <div className="flex justify-center space-x-8">
+            <div className="loader">
+              <svg viewBox="0 0 150 150">
+                <rect x="15" y="15" width="120" height="120"></rect>
+                <text
+                  x="50%"
+                  y="60%"
+                  textAnchor="middle"
+                  fill="black"
+                  fontSize="60"
+                  fontWeight="bold"
+                >
+                  D
+                </text>
+              </svg>
+            </div>
+
+            <div className="loader">
+              <svg viewBox="0 0 150 150">
+                <rect x="15" y="15" width="120" height="120"></rect>
+                <text
+                  x="50%"
+                  y="60%"
+                  textAnchor="middle"
+                  fill="black"
+                  fontSize="60"
+                  fontWeight="bold"
+                >
+                  R
+                </text>
+              </svg>
+            </div>
+
+            <div className="loader">
+              <svg viewBox="0 0 150 150">
+                <rect x="15" y="15" width="120" height="120"></rect>
+                <text
+                  x="50%"
+                  y="60%"
+                  textAnchor="middle"
+                  fill="black"
+                  fontSize="60"
+                  fontWeight="bold"
+                >
+                  A
+                </text>
+              </svg>
+            </div>
+
+            <div className="loader">
+              <svg viewBox="0 0 150 150">
+                <rect x="15" y="15" width="120" height="120"></rect>
+                <text
+                  x="50%"
+                  y="60%"
+                  textAnchor="middle"
+                  fill="black"
+                  fontSize="60"
+                  fontWeight="bold"
+                >
+                  G
+                </text>
+              </svg>
+            </div>
+
+            <div className="loader">
+              <svg viewBox="0 0 150 150">
+                <rect x="15" y="15" width="120" height="120"></rect>
+                <text
+                  x="50%"
+                  y="60%"
+                  textAnchor="middle"
+                  fill="black"
+                  fontSize="60"
+                  fontWeight="bold"
+                >
+                  O
+                </text>
+              </svg>
+            </div>
+
+            <div className="loader">
+              <svg viewBox="0 0 150 150">
+                <rect x="15" y="15" width="120" height="120"></rect>
+                <text
+                  x="50%"
+                  y="60%"
+                  textAnchor="middle"
+                  fill="black"
+                  fontSize="60"
+                  fontWeight="bold"
+                >
+                  N
+                </text>
+              </svg>
+            </div>
           </div>
 
-          <div className="loader">
-            <svg viewBox="0 0 150 150">
-              <rect x="15" y="15" width="120" height="120"></rect>
-              <text
-                x="50%"
-                y="60%"
-                textAnchor="middle"
-                fill="black"
-                fontSize="60"
-                fontWeight="bold"
-              >
-                R
-              </text>
-            </svg>
-          </div>
+          <div className="flex justify-center space-x-8">
+            <div className="loader">
+              <svg viewBox="0 0 150 150">
+                <rect x="15" y="15" width="120" height="120"></rect>
+                <text
+                  x="50%"
+                  y="60%"
+                  textAnchor="middle"
+                  fill="black"
+                  fontSize="60"
+                  fontWeight="bold"
+                >
+                  B
+                </text>
+              </svg>
+            </div>
 
-          <div className="loader">
-            <svg viewBox="0 0 150 150">
-              <rect x="15" y="15" width="120" height="120"></rect>
-              <text
-                x="50%"
-                y="60%"
-                textAnchor="middle"
-                fill="black"
-                fontSize="60"
-                fontWeight="bold"
-              >
-                A
-              </text>
-            </svg>
-          </div>
+            <div className="loader">
+              <svg viewBox="0 0 150 150">
+                <rect x="15" y="15" width="120" height="120"></rect>
+                <text
+                  x="50%"
+                  y="60%"
+                  textAnchor="middle"
+                  fill="black"
+                  fontSize="60"
+                  fontWeight="bold"
+                >
+                  A
+                </text>
+              </svg>
+            </div>
 
-          <div className="loader">
-            <svg viewBox="0 0 150 150">
-              <rect x="15" y="15" width="120" height="120"></rect>
-              <text
-                x="50%"
-                y="60%"
-                textAnchor="middle"
-                fill="black"
-                fontSize="60"
-                fontWeight="bold"
-              >
-                G
-              </text>
-            </svg>
-          </div>
+            <div className="loader">
+              <svg viewBox="0 0 150 150">
+                <rect x="15" y="15" width="120" height="120"></rect>
+                <text
+                  x="50%"
+                  y="60%"
+                  textAnchor="middle"
+                  fill="black"
+                  fontSize="60"
+                  fontWeight="bold"
+                >
+                  L
+                </text>
+              </svg>
+            </div>
 
-          <div className="loader">
-            <svg viewBox="0 0 150 150">
-              <rect x="15" y="15" width="120" height="120"></rect>
-              <text
-                x="50%"
-                y="60%"
-                textAnchor="middle"
-                fill="black"
-                fontSize="60"
-                fontWeight="bold"
-              >
-                O
-              </text>
-            </svg>
-          </div>
-
-          <div className="loader">
-            <svg viewBox="0 0 150 150">
-              <rect x="15" y="15" width="120" height="120"></rect>
-              <text
-                x="50%"
-                y="60%"
-                textAnchor="middle"
-                fill="black"
-                fontSize="60"
-                fontWeight="bold"
-              >
-                N
-              </text>
-            </svg>
-          </div>
-        </div>
-
-        <div className="flex justify-center space-x-8">
-          <div className="loader">
-            <svg viewBox="0 0 150 150">
-              <rect x="15" y="15" width="120" height="120"></rect>
-              <text
-                x="50%"
-                y="60%"
-                textAnchor="middle"
-                fill="black"
-                fontSize="60"
-                fontWeight="bold"
-              >
-                B
-              </text>
-            </svg>
-          </div>
-
-          <div className="loader">
-            <svg viewBox="0 0 150 150">
-              <rect x="15" y="15" width="120" height="120"></rect>
-              <text
-                x="50%"
-                y="60%"
-                textAnchor="middle"
-                fill="black"
-                fontSize="60"
-                fontWeight="bold"
-              >
-                A
-              </text>
-            </svg>
-          </div>
-
-          <div className="loader">
-            <svg viewBox="0 0 150 150">
-              <rect x="15" y="15" width="120" height="120"></rect>
-              <text
-                x="50%"
-                y="60%"
-                textAnchor="middle"
-                fill="black"
-                fontSize="60"
-                fontWeight="bold"
-              >
-                L
-              </text>
-            </svg>
-          </div>
-
-          <div className="loader">
-            <svg viewBox="0 0 150 150">
-              <rect x="15" y="15" width="120" height="120"></rect>
-              <text
-                x="50%"
-                y="60%"
-                textAnchor="middle"
-                fill="black"
-                fontSize="60"
-                fontWeight="bold"
-              >
-                L
-              </text>
-            </svg>
+            <div className="loader">
+              <svg viewBox="0 0 150 150">
+                <rect x="15" y="15" width="120" height="120"></rect>
+                <text
+                  x="50%"
+                  y="60%"
+                  textAnchor="middle"
+                  fill="black"
+                  fontSize="60"
+                  fontWeight="bold"
+                >
+                  L
+                </text>
+              </svg>
+            </div>
           </div>
         </div>
       </div>
--- src/features/dragon-ball/components/dragon-ball-card.tsx
+++ src/features/dragon-ball/components/dragon-ball-card.tsx
@@ -7,19 +7,65 @@ interface DragonBallPlanetsDetailProps {
 export default function DragonBallPlanetsDetailComponent({
   planet,
 }: DragonBallPlanetsDetailProps) {
-  return (
-    <div className="grid grid-cols-[40%_60%] min-h-screen justify-items-center p-8 space-x-4">
-      <div className="flex flex-col justify-center items-center border-r-2 border-gray-300 pr-4">
-        <h1 className="text-2xl font-bold mb-4">{planet.name}</h1>
-        <img
-          src={planet.image}
-          alt={planet.name}
-          className="h-126 w-auto object-cover object-top"
-        />
-      </div>
+  const firstRowCharacters = planet.characters.slice(0, 8); // Primeros 8 personajes
+  const secondRowCharacters = planet.characters.slice(8, 16); // Siguientes 8 personajes
 
-      <div className="flex justify-center items-center">
-        <p className="text-lg text-center">{planet.description}</p>
+  return (
+    <div
+      className="flex items-center justify-center min-h-screen bg-orange-500"
+      style={{
+        backgroundImage:
+          "repeating-conic-gradient(from 0deg, orangered, transparent 10deg, orange 10deg, orangered 20deg)",
+        backgroundSize: "100% 100%",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: "orange",
+      }}
+    >
+      <div className="w-[90vw] h-[80vh] bg-yellow-500 rounded-lg shadow-lg overflow-hidden border-4 border-orange-600">
+        <div className="grid grid-cols-1 md:grid-cols-2 min-h-full p-4">
+          <div className="flex justify-center items-center mb-4 md:mb-0">
+            <img
+              src={planet.image}
+              alt={planet.name}
+              className="h-102 w-102 object-cover rounded-lg shadow-md"
+            />
+          </div>
+
+          <div className="flex flex-col justify-center items-start text-white">
+            <h1 className="text-3xl font-bold mb-4">{planet.name}</h1>
+            <p className="text-lg">{planet.description}</p>
+            <br />
+            <p className=" font-bold text-3xl mb-4">
+              Personajes que hacen parte de este planeta:
+            </p>
+
+            <ul className="flex space-x-10">
+              {firstRowCharacters.map((characters) => (
+                <li key={planet.id} className="flex flex-col items-center">
+                  <img
+                    src={characters.image}
+                    alt={characters.name}
+                    className="h-18 w-18 object-cover object-top rounded-full mb-2"
+                  />
+                  <span className="text-lg text-center">{characters.name}</span>
+                </li>
+              ))}
+            </ul>
+
+            <ul className="flex space-x-10">
+              {secondRowCharacters.map((characters) => (
+                <li key={planet.id} className="flex flex-col items-center">
+                  <img
+                    src={characters.image}
+                    alt={characters.name}
+                    className="h-18 w-18 object-cover object-top rounded-full mb-2"
+                  />
+                  <span className="text-lg text-center">{characters.name}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
